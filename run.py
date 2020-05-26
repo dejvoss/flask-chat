6b6766aa99f946bfbc82fcf6e94634d139cd529e
dejvoss <deosiecki@gmail.com>
2020-05-27 00:25:47 +0200

    Add comment about port settings

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ def index():
     return "<h1>Hello there</h1>"
 
 
-# remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku
+# remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku or you want to use it on the gitpod
 app.run(host=os.getenv('IP'), port='6080', debug=True)
