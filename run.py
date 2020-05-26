e542b19f5bd70b2aa33de8acf756efd901ace4d2
dejvoss <deosiecki@gmail.com>
2020-05-26 23:05:40 +0000

    Add usernam and message app route

--- run.py
+++ run.py
@@ -6,8 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello there</h1>"
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
 
 
-# remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku or you want to use it on the gitpod
+@app.route('/<username>')
+def user(username):
+    return "Hi" + username
+
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+"""remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku or you want to use it on the gitpod"""
 app.run(host=os.getenv('IP'), port='6080', debug=True)
