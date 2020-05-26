820cb9bdbb7de9b13c20ad994a60564cdd1524f9
dejvoss <deosiecki@gmail.com>
2020-05-26 23:19:11 +0000

    Add the ability to write to and read from a list and display the massegas to the user

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_messages(username, message):
+    """Add messages to the messages list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all of the messages and seperate them with a br """
+    return "<br>".join(messages)
 
 @app.route('/')
 def index():
@@ -12,12 +20,15 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi" + username
+    """Display chat message"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 """remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku or you want to use it on the gitpod"""
 app.run(host=os.getenv('IP'), port='6080', debug=True)
