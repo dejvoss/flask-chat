ebdbb2b4fd80a8bfbac4091a9e293bf65225d5c5
dejvoss <deosiecki@gmail.com>
2020-05-27 00:24:53 +0200

    Fix the errors

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "<h1>Hello there</h1>"
-#remember to change port before you will deploy app in to heroku
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')) debug=True) 
+
+
+# remember to change port for 'port=int(os.getenv('PORT'))'before you will deploy app in to heroku
+app.run(host=os.getenv('IP'), port='6080', debug=True)
