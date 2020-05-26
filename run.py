b35a2e0829843f4c0db6adcd869bd2f1584aa5f4
dejvoss <deosiecki@gmail.com>
2020-05-26 23:36:27 +0000

    Add the username form to the index and display and storage username in cokie by request and session flask features

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -14,9 +15,16 @@ def get_all_messages():
     """Get all of the messages and seperate them with a br """
     return "<br>".join(messages)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 
